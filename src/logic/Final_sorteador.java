635811ef869773577edb724d56373a4cdebb777e
Dottiewh <domax.dott@gmail.com>
2025-07-26 00:23:36 -0400

    Release 1.0

---
+++ src/logic/Final_sorteador.java
@@ -0,0 +1,11 @@
+package logic;
+import gui.Principal;
+
+public class Final_sorteador {
+    public static void main(String[] args) {
+        Principal gui = new Principal();
+        gui.setVisible(true);   
+        gui.setLocationRelativeTo(null);
+        
+    }
+}
